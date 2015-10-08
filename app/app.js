c84345179a63961660e58e121515d4ac56d9a369
Thomas Schertenleib <thomas.schertenleib@gmail.com>
2015-10-08 16:25:56 +0200

    angular init

---
+++ app/app.js
@@ -0,0 +1,24 @@
+// create a new angular app
+var app = angular.module('InCartasApp', ['ngTouch', 'ngAnimate', 'ngRoute']);
+
+// setup router
+app.config(['$routeProvider', function($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'views/medikamente.html'
+    })
+    .when('/:id', {
+      templateUrl: 'views/profil.html'
+    });
+}]);
+
+// set initial list items
+app.value('items',[
+  { name: 'Buy a nice dog house', finished: true },
+  { name: 'Get a dog from the pet shop', finished: false },
+  { name: 'Order some proper dog food', finished: false }
+]);
+
+// load controllers
+require('controllers/medikamente')(app);
+require('controllers/profil')(app);
--- app/directory.js
+++
@@ -1,7 +0,0 @@
-var app = angular.module('InCartas', [])
-	.controller('directoryController', function($scope) {
-	$scope.list = [
-			{name:'Aspirin', tag: 'Schmerzen'},
-			{name:'Voltaren', tag: 'Wunderkrankungen'}
-	]
-	});
